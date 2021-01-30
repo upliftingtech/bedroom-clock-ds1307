cb13101031eceac4c85016455ef2086816ad2f60
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-01-29 23:09:22 -0800

    add if loopChrono.hasPassed check to main loop
    this is what creates the 1 second timing pause now

--- bedroom-clock-ds1307.ino
+++ bedroom-clock-ds1307.ino
@@ -95,101 +95,105 @@ void setup() {
 }
 
 void loop() {
-  // 
+  // check to see if it is time to run the loop
+  if (loopChrono.hasPassed(1000) ) { // is true if 1000ms have passed since chronometer was started
+      loopChrono.restart();  // start/restarts the chronometer
 
-  // Loop function runs over and over again to implement the clock logic.
-  
-  // Check if it's the top of the hour and get a new time reading
-  // from the DS1307.  This helps keep the clock accurate by fixing
-  // any drift.
-  if (minutes == 0) {
-    // Get the time from the DS1307.
-    DateTime now = rtc.now();
-    // Print out the time for debug purposes:
-    Serial.print("Read date & time from DS1307: ");
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(' ');
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    // Now set the hours and minutes.
-    hours = now.hour();
-    minutes = now.minute();
-  }
-
-  // Show the time on the display by turning it into a numeric
-  // value, like 3:30 turns into 330, by multiplying the hour by
-  // 100 and then adding the minutes.
-  int displayValue = hours*100 + minutes;
-
-  // Do 24 hour to 12 hour format conversion when required.
-  if (!TIME_24_HOUR) {
-    // Handle when hours are past 12 by subtracting 12 hours (1200 value).
-    if (hours > 12) {
-      displayValue -= 1200;
-    }
-    // Handle hour 0 (midnight) being shown as 12.
-    else if (hours == 0) {
-      displayValue += 1200;
-    }
-  }
-
-  // Now print the time value to the display.
-  clockDisplay.print(displayValue, DEC);
-
-  // Add zero padding when in 24 hour mode and it's midnight.
-  // In this case the print function above won't have leading 0's
-  // which can look confusing.  Go in and explicitly add these zeros.
-  if (TIME_24_HOUR && hours == 0) {
-    // Pad hour 0.
-    clockDisplay.writeDigitNum(1, 0);
-    // Also pad when the 10's minute is 0 and should be padded.
-    if (minutes < 10) {
-      clockDisplay.writeDigitNum(2, 0);
-    }
-  }
-
-  // Blink the colon by flipping its value every loop iteration
-  // (which happens every second).
-  blinkColon = !blinkColon;
-  clockDisplay.drawColon(blinkColon);
-
-  // Now push out to the display the new values that were set above.
-  clockDisplay.writeDisplay();
-
-  // Pause for a second for time to elapse.  This value is in milliseconds
-  // so 1000 milliseconds = 1 second.
-//  delay(1000);
-// can't use delay and do wifi
-
-  // Now increase the seconds by one.
-  seconds += 1;
-  // If the seconds go above 59 then the minutes should increase and
-  // the seconds should wrap back to 0.
-  if (seconds > 59) {
-    seconds = 0;
-    minutes += 1;
-    // Again if the minutes go above 59 then the hour should increase and
-    // the minutes should wrap back to 0.
-    if (minutes > 59) {
-      minutes = 0;
-      hours += 1;
-      // Note that when the minutes are 0 (i.e. it's the top of a new hour)
-      // then the start of the loop will read the actual time from the DS1307
-      // again.  Just to be safe though we'll also increment the hour and wrap
-      // back to 0 if it goes above 23 (i.e. past midnight).
-      if (hours > 23) {
-        hours = 0;
-      }
-    }
-  }
+	  // Loop function runs over and over again to implement the clock logic.
+	  
+	  // Check if it's the top of the hour and get a new time reading
+	  // from the DS1307.  This helps keep the clock accurate by fixing
+	  // any drift.
+	  if (minutes == 0) {
+	    // Get the time from the DS1307.
+	    DateTime now = rtc.now();
+	    // Print out the time for debug purposes:
+	    Serial.print("Read date & time from DS1307: ");
+	    Serial.print(now.year(), DEC);
+	    Serial.print('/');
+	    Serial.print(now.month(), DEC);
+	    Serial.print('/');
+	    Serial.print(now.day(), DEC);
+	    Serial.print(' ');
+	    Serial.print(now.hour(), DEC);
+	    Serial.print(':');
+	    Serial.print(now.minute(), DEC);
+	    Serial.print(':');
+	    Serial.print(now.second(), DEC);
+	    Serial.println();
+	    // Now set the hours and minutes.
+	    hours = now.hour();
+	    minutes = now.minute();
+	  }
+	
+	  // Show the time on the display by turning it into a numeric
+	  // value, like 3:30 turns into 330, by multiplying the hour by
+	  // 100 and then adding the minutes.
+	  int displayValue = hours*100 + minutes;
+	
+	  // Do 24 hour to 12 hour format conversion when required.
+	  if (!TIME_24_HOUR) {
+	    // Handle when hours are past 12 by subtracting 12 hours (1200 value).
+	    if (hours > 12) {
+	      displayValue -= 1200;
+	    }
+	    // Handle hour 0 (midnight) being shown as 12.
+	    else if (hours == 0) {
+	      displayValue += 1200;
+	    }
+	  }
+	
+	  // Now print the time value to the display.
+	  clockDisplay.print(displayValue, DEC);
+	
+	  // Add zero padding when in 24 hour mode and it's midnight.
+	  // In this case the print function above won't have leading 0's
+	  // which can look confusing.  Go in and explicitly add these zeros.
+	  if (TIME_24_HOUR && hours == 0) {
+	    // Pad hour 0.
+	    clockDisplay.writeDigitNum(1, 0);
+	    // Also pad when the 10's minute is 0 and should be padded.
+	    if (minutes < 10) {
+	      clockDisplay.writeDigitNum(2, 0);
+	    }
+	  }
+	
+	  // Blink the colon by flipping its value every loop iteration
+	  // (which happens every second).
+	  blinkColon = !blinkColon;
+	  clockDisplay.drawColon(blinkColon);
+	
+	  // Now push out to the display the new values that were set above.
+	  clockDisplay.writeDisplay();
+	
+	  // Pause for a second for time to elapse.  This value is in milliseconds
+	  // so 1000 milliseconds = 1 second.
+	  //  delay(1000);
+	  // can't use delay and do wifi
+	
+	  // Now increase the seconds by one.
+	  seconds += 1;
+	  // If the seconds go above 59 then the minutes should increase and
+	  // the seconds should wrap back to 0.
+	  if (seconds > 59) {
+	    seconds = 0;
+	    minutes += 1;
+	    // Again if the minutes go above 59 then the hour should increase and
+	    // the minutes should wrap back to 0.
+	    if (minutes > 59) {
+	      minutes = 0;
+	      hours += 1;
+	      // Note that when the minutes are 0 (i.e. it's the top of a new hour)
+	      // then the start of the loop will read the actual time from the DS1307
+	      // again.  Just to be safe though we'll also increment the hour and wrap
+	      // back to 0 if it goes above 23 (i.e. past midnight).
+	      if (hours > 23) {
+	        hours = 0;
+	      }
+	    }
+	  }
+	  
+	} // endif check for chrono
 
   // Loop code is finished, it will jump back to the start of the loop
   // function again!
